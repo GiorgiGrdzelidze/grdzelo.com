396364ced43000699b703b485eaf16a404b82363
giorgi grdzelidze <grdzelog@gmail.com>
2026-05-01 14:14:29 +0400

    fix(seo): harden JSON-LD encoding (UNESCAPED_SLASHES/UNICODE + HEX_TAG)

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -124,9 +124,13 @@
                 <meta name="twitter:image:alt" content="{{ $twitter['image_alt'] }}">
             @endif
         @endif
-        {{-- JSON-LD Schema --}}
+        {{-- JSON-LD Schema. Flags:
+             - UNESCAPED_SLASHES keeps URLs readable (no \/ in canonical/url fields).
+             - UNESCAPED_UNICODE keeps Georgian/Cyrillic intact in name/description for crawlers.
+             - HEX_TAG escapes < and > so a stray "</script>" inside admin content
+               can't break out of the script element. --}}
         @if($schema)
-            <script type="application/ld+json">{!! json_encode($schema) !!}</script>
+            <script type="application/ld+json">{!! json_encode($schema, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE | JSON_HEX_TAG) !!}</script>
         @endif
     </head>
     <body class="font-sans antialiased">
